8e7cc9f39e9c6bcfe0defbb593266bd3d61f8546
MMShed <88801787+MMShed@users.noreply.github.com>
2025-08-27 13:52:03 -0400

    You can now play a round

--- script.js
+++ script.js
@@ -25,10 +25,84 @@ function getComputerChoice ()
 function getHumanChoice ()
 {
     let choice = window.prompt();
-    return choice;
+    return choice.toLowerCase();
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
+/*
+    1) Take the choice made from both computer and human
+    2) Show in the console who made what choice, and whether you win or not
+    3) Show the scores after each round
+*/
 
+function playRound (humanChoice, computerChoice)
+{
+    console.log (`You chose ${humanChoice}. The computer chose ${computerChoice}.`);
+
+    if (humanChoice === "rock") 
+    {
+        switch (computerChoice)
+        {
+            case "rock":
+                console.log ("It's a tie!");
+                break;
+            
+            case "paper":
+                console.log ("You win!");
+                humanScore++;
+                break;
+            
+            case "scissors":
+                console.log ("Computer wins!");
+                computerScore++;
+                break;
+        }
+    }
+
+    else if (humanChoice === "paper") 
+    {
+        switch (computerChoice)
+        {
+            case "rock":
+                console.log ("You win!");
+                humanScore++;
+                break;
+            
+            case "paper":
+                console.log ("It's a tie!");
+                break;
+            
+            case "scissors":
+                console.log ("Computer wins!");
+                computerScore++;
+                break;
+        }
+    }
+
+    else /*Scissors*/
+    {
+        switch (computerChoice)
+        {
+            case "rock":
+                console.log ("Computer wins!");
+                computerScore++;
+                break;
+            
+            case "paper":
+                console.log ("It's a tie!");
+                break;
+            
+            case "scissors":
+                console.log ("You win!");
+                humanScore++;
+                break;
+        }
+    }
+
+    console.log (`Human: ${humanScore}   Computer: ${computerScore}`)
+}
+
+
+playRound(getHumanChoice(), getComputerChoice());
